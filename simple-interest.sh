7b45eece037a0f86f918c852f5d09af7fcf030f8
Yekkanti Mahesh Reddy <119095805+Mahi-0708@users.noreply.github.com>
2025-09-02 14:54:49 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# simple-interest.sh
+# Formula: SI = (P * T * R) / 100
+
+echo "Enter Principal:"
+read P
+echo "Enter Time (in years):"
+read T
+echo "Enter Rate of Interest:"
+read R
+
+SI=$((P * T * R / 100))
+
+echo "Simple Interest = $SI"
